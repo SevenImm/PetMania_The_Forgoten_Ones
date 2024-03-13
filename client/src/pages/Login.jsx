c83bfe8a6201a86189ab4f2ee4f91e9c3fca4e00
Jeremy <jbjarvis@mac.com>
2024-03-13 00:14:31 -0500

    made login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -41,54 +41,74 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+    <div className="flex justify-center items-center h-screen 
+    bg-gradient-to-b from-sky-500 from-10% via-green-700 via-90% to bg-yellow-700">
+  <div className="w-full max-w-md">
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+        Login
+      </h2>
+      {data ? (
+        <p className="text-green-500 mb-4">
+          Success! You may now head{' '}
+          <Link to="/" className="text-blue-500">
+            back to the homepage.
+          </Link>
+        </p>
+      ) : (
+        <form onSubmit={handleFormSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="form-input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Your email"
+              value={formState.email}
+              onChange={handleChange}
+            />
           </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="form-input shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="******"
+              value={formState.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            style={{ cursor: 'pointer' }}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      )}
+
+      {error && (
+        <div className="my-3 p-3 bg-red-500 text-white rounded">
+          {error.message}
         </div>
-      </div>
-    </main>
-  );
-};
+      )}
+    </div>
+  </div>
+</div>
+)};
 
 export default Login;
